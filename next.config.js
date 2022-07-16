88dd49a5d5ebdea76fde541a240d1417fac78a35
elisheba-beep <ekunife35@gmail.com>
2022-07-16 22:25:10 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-}
-
-module.exports = nextConfig
-
-module.exports = {
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-    }
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
+      "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
+      "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } },
+    };
   },
-}
-module.exports = {
   trailingSlash: true,
-}
+};
+
+module.exports = nextConfig;
